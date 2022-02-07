4f67a4524ffc0d0dcf34ab7ab5e886a25425d359
Jul10CC <julio1cayax@outlook.es>
2022-02-06 20:23:23 -0600

    Configuracion de conexion con la Base de Datos
    MySql

--- mpbackend/src/main/java/com/example/mpbackend/model/Fiscalia.java
+++ mpbackend/src/main/java/com/example/mpbackend/model/Fiscalia.java
@@ -1,4 +1,54 @@
 package com.example.mpbackend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Fiscalia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int id;
+    private String name;
+    private String address;
+    private String number;
+
+    public Fiscalia() {
+
+    }
+
+    //Generacion de Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+
+    public java.lang.String getAddress() {
+        return address;
+    }
+
+    public void setAddress(java.lang.String address) {
+        this.address = address;
+    }
+
+    public java.lang.String getNumber() {
+        return number;
+    }
+
+    public void setNumber(java.lang.String number) {
+        this.number = number;
+    }
 }
--- mpbackend/src/main/java/com/example/mpbackend/repository/FiscaliaRepository.java
+++ mpbackend/src/main/java/com/example/mpbackend/repository/FiscaliaRepository.java
@@ -1,4 +1,10 @@
 package com.example.mpbackend.repository;
+import com.example.mpbackend.model.Fiscalia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface FiscaliaRepository {
+@Repository
+public interface FiscaliaRepository extends JpaRepository<Fiscalia, Integer> {
+
+
 }
